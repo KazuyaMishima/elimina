24f0b6a11e07002b3f0bb626e793ee72472ed203
Maorda <ingdantev@gmail.com>
2017-06-30 23:47:39 +0200

    Initial commit

---
+++ app/GraphQL/Mutation/CreateUserMutation.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\GraphQL\Mutation;
+
+use App\User;
+use Folklore\GraphQL\Support\Mutation;
+use GraphQL\Type\Definition\ResolveInfo;
+use GraphQL\Type\Definition\Type;
+use GraphQL;
+
+class CreateUserMutation extends Mutation
+{
+    protected $attributes = [
+        'name' => 'CreateUser',
+        'description' => 'User mutation'
+    ];
+
+    public function type()
+    {
+        return GraphQL::type('User');
+    }
+
+    public function args()
+    {
+        return [
+            'name'=>[
+                'type'=>Type::nonNull(Type::string())
+            ],
+            'email'=>[
+                'type'=>Type::nonNull(Type::string()),
+                'rules'=>['email','unique:users']
+            ],
+            'password'=>[
+                'type'=>Type::nonNull(Type::string()),
+                'rules'=>['min:4']
+            ]
+        ];
+    }
+
+    public function resolve($root, $args, $context, ResolveInfo $info)
+    {
+        $user = new User();
+        $user->fill($args);
+        $user->password = bcrypt($args['password']);
+        return $user;
+
+    }
+}
